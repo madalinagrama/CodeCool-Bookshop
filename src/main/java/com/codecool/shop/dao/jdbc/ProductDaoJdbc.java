14f85e661a8e08c64a6140b483e66d7bd1cf4103
madalinagrama <m.madalinagrama@gmail.com>
2021-08-24 16:12:38 +0300

    ProductDaoJdbc added

--- src/main/java/com/codecool/shop/dao/jdbc/ProductDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/jdbc/ProductDaoJdbc.java
@@ -1,6 +1,38 @@
 package com.codecool.shop.dao.jdbc;
 
-public class ProductDaoJdbc {
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.model.ProductCategory;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductDaoJdbc implements ProductCategoryDao {
+    private DataSource dataSource;
+
+    public ProductDaoJdbc(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
 
+    @Override
+    public void add(ProductCategory category) {
+
+    }
+
+    @Override
+    public ProductCategory find(int id) {
+        return null;
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
+
+    @Override
+    public List<ProductCategory> getAll() {
+        return null;
+    }
 }
